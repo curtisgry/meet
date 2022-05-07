33c0cd9c69146a4e48065820cad113bbd14f3e77
Curtis Gray <87861225+curtisgry@users.noreply.github.com>
2022-05-07 09:59:07 -0400

    add cors to axios events get request

--- src/api.js
+++ src/api.js
@@ -48,7 +48,7 @@ export const getEvents = async () => {
       'https://dau1cz6z6a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
       '/' +
       'token';
-    const result = await axios.get(url);
+    const result = await axios.get(url, {headers: {'Access-Control-Allow-Origin': '*'}});
     if (result.data) {
       const locations = extractLocations(result.data.events);
       localStorage.setItem('lastEvents', JSON.stringify(result.data));
